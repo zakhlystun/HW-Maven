7df356795c3193679bb6648167e2e930249730bf
Sergey Zakhlystun <zakhlystun@gmail.com>
2023-12-08 15:00:43 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service;
+        service = new BonusService();
+        long bonus = service.calculate(1000, true);
+        System.out.println(bonus);
+    }
+}
